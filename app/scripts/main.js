103c09c8ac0e3eb9fb658958d990b5a5449ffc58
Scott Markley <smarkley@bignerdranch.com>
2013-05-04 13:41:53 -0400

    Alarm works! Yeaah!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,6 +14,5 @@ require.config({
 require(['app', 'jquery', 'bootstrap'], function (app, $) {
     'use strict';
     // use app here
-    console.log(app);
-    console.log('Running jQuery %s', $().jquery);
+
 });
